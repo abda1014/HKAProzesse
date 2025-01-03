2d23068dd237aaf61ea1f33bcfc2bb598ec5cc91
David Abraham <127962371+abda1014@users.noreply.github.com>
2025-01-03 13:58:15 +0100

    update

--- src/main/java/com/acme/hkaprozesse/service/AccountingRoleService.java
+++ src/main/java/com/acme/hkaprozesse/service/AccountingRoleService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+
 @Service
 public class AccountingRoleService {
 
